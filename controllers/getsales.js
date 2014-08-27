f8a3d20ea8d8a46a1645948af1ad7499e9caf39a
michael colenso <github@michaelcolenso.com>
2014-08-27 01:44:11 -0400

    seed the db for dokku

--- controllers/getsales.js
+++ controllers/getsales.js
@@ -2,10 +2,7 @@ var secrets = require('../config/secrets');
 var request = require('request');
 var fs = require('fs');
 var parser = require('JSONStream').parse('features.*.attributes')
-var options = { db: secrets.db,  collection: 'tcsalesdata' };
+var options = { db: secrets.db,  collection: 'id_county_item' };
 var streamToMongo = require('stream-to-mongo')(options);
 
-fs.createReadStream(__dirname + '/salesdatatc.json').pipe(parser).pipe(streamToMongo);
-//request("http://arcserver.tclp.org/arcgis/rest/services/City/CityParcelViewer/MapServer/2/query?where=objectid+%3D+objectid&outfields=*&f=json")
-  //.pipe(parser)
-  //.pipe(streamToMongo);
+fs.createReadStream(__dirname + '/id_county_item.json').pipe(parser).pipe(streamToMongo);
