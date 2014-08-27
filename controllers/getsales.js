fc858ec4d0147a2e98fd6fb4a5591227932a5ad4
michael colenso <github@michaelcolenso.com>
2014-08-27 01:59:27 -0400

    dokku

--- controllers/getsales.js
+++ controllers/getsales.js
@@ -4,5 +4,4 @@ var fs = require('fs');
 var parser = require('JSONStream').parse('features.*.attributes')
 var options = { db: secrets.db,  collection: 'id_county_item' };
 var streamToMongo = require('stream-to-mongo')(options);
-
 fs.createReadStream(__dirname + '/id_county_item.json').pipe(parser).pipe(streamToMongo);
