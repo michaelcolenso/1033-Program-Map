8a0aaec7eae83bf3eb95de4923d453cc248a2604
michaelcolenso <github@michaelcolenso.com>
2017-06-14 14:14:01 -0700

    removed public folder

--- app.js
+++ app.js
@@ -105,7 +105,7 @@ app.use(function(req, res, next) {
   req.session.returnTo = req.path;
   next();
 });
-app.use(express.static(path.join(__dirname, 'public'), { maxAge: week }));
+app.use(express.static(path.join(__dirname, 'public'), { maxAge: day }));
 
 /**
  * routes.
